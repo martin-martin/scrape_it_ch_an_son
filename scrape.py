1518d8a2a2d62efa7785ab4187889349de096b06
martin-martin <breuss.martin@gmail.com>
2017-02-24 08:16:23 -0500

    add missing slash char

--- scrape.py
+++ scrape.py
@@ -1,25 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = "http://api.genius.com"
-
-song_title = "Lake Song"
-artist_name = "The Decemberists"
+song_title = "Traicionera"
+artist_name = "Sebastian Yatra"
 
 def linify(a_string):
+  """Replaces whitespace chars in a string for dashes."""
   linified = a_string.replace(" ", "-")
   return linified
 
 def scrape(artist_name, song_title):
-  # getting the URL right
+  """Scrapes the lyrics of a specified song from the Genius website.
+
+  :param artist_name: name of the artist
+  :type artist_name: str
+  :param song_title: title of the song
+  :type song_title: str
+  :return: lyrics for the given song
+  :rtype: str (including newline chars)
+  """
+  # the URL uses dashes to separate words
+  # and is built in the format 'baseURL/artist-song-lyrics'
   path = linify(artist_name) + "-" + linify(song_title) + "-lyrics"
-  page_url = "http://genius.com" + path
+  page_url = "http://genius.com/" + path
   page = requests.get(page_url)
   html = BeautifulSoup(page.text, "html.parser")
   # remove script tags that they put in the middle of the lyrics
   [h.extract() for h in html('script')]
-  # at least Genius is nice and has a tag called 'lyrics'!
-  lyrics = html.find("lyrics").get_text
+  # Genius has a tag called 'lyrics'!
+  lyrics = html.find("lyrics").get_text()
   return lyrics
 
 print scrape(artist_name, song_title)
